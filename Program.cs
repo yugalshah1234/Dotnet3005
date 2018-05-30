9852225c952742bf4eb5ac6dcdcd9bf30d1a633f
yugalshah1234 <35334001+yugalshah1234@users.noreply.github.com>
2018-05-30 14:43:41 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnetcore_stacktrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new RaiseException("");
+            new RaiseException("Custom exception");
+            Console.WriteLine("Logs sent to loggly. Press any key to exit application");
+            Console.ReadKey();
+        }
+    }
+}
